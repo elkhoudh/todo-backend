bafa79d49fec92e6785a5d3b627e35acd5dbe306
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-17 16:41:35 -0500

    first commit

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ server.use(helmet());
 
 server.use("/api/todos", todoRouter);
 
+server.get("*", (req, res) => {
+  res.send(
+    "<h1 style='color: red; text-align: center;'>TODO BACKEND FOR REACT NATIVE APPLICATION</h1>"
+  );
+});
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log("Listening on port 5000..."));

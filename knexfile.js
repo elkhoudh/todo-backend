4497260a48e9235789edf93563ac9d359f0a8d80
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-18 11:36:14 -0500

    PostGres

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,8 @@
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: { filename: "./todo.db3" },
+    client: "pg",
+    connection:
+      "postgres://vgvrezkllbvrzw:c0c30206419ad979a490fb253e957711e8a01d0ac570420ae69fd409dfd41f8a@ec2-54-163-234-88.compute-1.amazonaws.com:5432/defes24td2hnbb",
     useNullAsDefault: true
   }
 };
